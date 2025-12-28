1be0a83bce91820efb4fd721e5782510affe697e
pkb-pmj <pkb.pmj@gmail.com>
2025-12-28 19:08:31 +0100

    test on complex equation system

--- script.m
+++ script.m
@@ -16,3 +16,17 @@ Y = eye(n);
 X1 = GECP(A, Y);
 X2 = linsolve(A, Y);
 max(abs(X1 - X2), [], "all")
+%% test4: complex
+n = 2;
+A = rand([n,n]);
+B = rand([n,n]);
+a = rand([n,1]);
+b = rand([n,1]);
+C = A + 1i*B;
+c = a + 1i*b;
+M = [A, -B; B, A];
+m = [a; b];
+z2 = GECP(M, m);
+z2 = z2(1:n) + 1i*z2(n+1:2*n);
+z1 = linsolve(C, c);
+max(abs(z1 - z2))
