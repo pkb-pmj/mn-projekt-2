d145a71427489ebdf891f12c77b3e2d63b14b6f9
AntoniKobosz <Antoni.Kobosz@gmail.com>
2026-01-02 17:37:50 +0100

    Test 6

--- script.m
+++ script.m
@@ -44,3 +44,47 @@ z2 = GEPP(M, m);
 z2 = z2(1:n) + 1i*z2(n+1:2*n);
 z1 = linsolve(C, c);
 max(abs(z1 - z2))
+%% test6: Czy trik ze macierzą blokową coś daje?
+n = 3;
+A = rand([n,n]);
+B = rand([n,n]);
+a = rand([n,1]);
+b = rand([n,1]);
+C = A + 1i*B;
+c = a + 1i*b;
+M = [A,-B;B,A];
+m = [a; b];
+t = zeros(5,1);
+tic
+z0 = linsolve(C, c); % wbudowana funkcja
+t(1) = toc;
+tic
+z1 = GECP(C,c); % GECP na zespolonej
+t(2) = toc;
+tic
+z2 = GECP(M, m); % GECP z użyciem macierzy blokowej
+z2 = z2(1:n) + 1i*z2(n+1:2*n);
+t(3) = toc;
+tic
+z3 = GEPP(C,c); % GEPP na zespolonej
+t(4) = toc;
+tic
+z4 = GEPP(M, m); % GEPP z użyciem macierzy blokowej
+z4 = z4(1:n) + 1i*z4(n+1:2*n);
+t(5) = toc;
+
+E = zeros(5,1);
+p = Inf;
+E(1) = norm(C*z0-c,p);
+E(2) = norm(C*z1-c,p);
+E(3) = norm(C*z2-c,p);
+E(4) = norm(C*z3-c,p);
+E(5) = norm(C*z4-c,p);
+
+format short e
+disp('Porównanie błędów ||C*z-c|| dla różnych metod')
+disp(table( ...
+    categorical(["linsolve";"GECP(C,c)";"GECP(M, m)";"GEPP(C,c)";"GEPP(M, m)"]), ...
+    t, ...
+    E, ...
+    'VariableNames',{'Metoda','Czas',sprintf('norm(C*z-c,%g)',p)}))
